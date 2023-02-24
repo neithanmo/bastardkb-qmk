0b8170e6a28eb6e86ce012671f419726ca6fc221
neithanmo <neithanmo@gmail.com>
2023-02-23 18:36:28 -0600

    dilema keymap

---
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/neithanmo/config.h
@@ -0,0 +1,100 @@
+/**
+ * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
+ * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#ifdef VIA_ENABLE
+/* Via configuration. */
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#endif // VIA_ENABLE
+
+/**
+ * Configure the global tapping term (default: 200ms).
+ * If you have a lot of accidental mod activations, crank up the tapping term.
+ *
+ * See docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-term
+ */
+#ifndef TAPPING_TERM
+#    define TAPPING_TERM 280
+#endif // TAPPING_TERM
+//
+#define PERMISSIVE_HOLD
+
+#define IGNORE_MOD_TAP_INTERRUPT
+// #define HOLD_ON_OTHER_KEY_PRESS
+#define TAPPING_FORCE_HOLD
+#define RETRO_TAPPING
+#define ONESHOT_TAP_TOGGLE 2 /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000
+
+// disable trackpad taps
+// #define CIRQUE_PINNACLE_TAPPING_TERM 0
+
+/**
+ * Enable rapid switch from tap to hold.  Disable auto-repeat when pressing key
+ * twice, except for one-shot keys.
+ *
+ * See docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-force-hold
+ */
+// #define TAPPING_FORCE_HOLD
+
+/*
+ * Tap-or-Hold decision modes.
+ *
+ * Note that the following flags behave differently when combined (ie. when 2 or
+ * more are enabled).
+ *
+ * See bit.ly/tap-or-hold for a visual explanation of the following tap-or-hold
+ * decision modes.
+ */
+
+/**
+ * Faster tap-hold trigger.
+ *
+ * Without `PERMISSIVE_HOLD`, within `TAPPING_TERM`:
+ *   Mod(a)🠗 e🠗 e🠕 Mod(a)🠕 ➞ ae
+ * With `PERMISSIVE_HOLD`, within `TAPPING_TERM`:
+ *   Mod(a)🠗 e🠗 e🠕 Mod(a)🠕 ➞ Mod+e
+ *
+ * See docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold
+ */
+// #define PERMISSIVE_HOLD
+
+/**
+ * Prevent normal rollover on alphas from accidentally triggering mods.
+ *
+ * Ignores key presses that interrupt a mod-tap.  Must-have for Home Row mod.
+ *
+ * Without `IGNORE_MOD_TAP_INTERRUPT`, within `TAPPING_TERM`:
+ *   Mod(a)🠗 e🠗 Mod(a)🠕 e🠕 ➞ Mod+e
+ * With `IGNORE_MOD_TAP_INTERRUPT`, within `TAPPING_TERM`:
+ *   Mod(a)🠗 e🠗 Mod(a)🠕 e🠕 ➞ ae
+ *
+ * See docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
+ */
+// #define IGNORE_MOD_TAP_INTERRUPT
+
+/** Dilemma-specific features. */
+
+#ifdef POINTING_DEVICE_ENABLE
+// Flip horizontal direction for drag-scroll.
+#    define DILEMMA_DRAGSCROLL_REVERSE_X
+// #define DILEMMA_DRAGSCROLL_REVERSE_Y
+#endif // POINTING_DEVICE_ENABLE
+
+#define COMBO_MUST_HOLD_MODS
+#define COMBO_TERM 60
---
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/neithanmo/keymap.c
@@ -0,0 +1,235 @@
+/**
+ * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
+ * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum keymap_layers {
+    LAYER_BASE = 0,
+    LAYER_SYM,
+    LAYER_NUM,
+    LAYER_NAV,
+    LAYER_FUN,
+};
+
+// Automatically enable sniping when the mouse layer is on.
+#define DILEMMA_AUTO_SNIPING_ON_LAYER LAYER_NAV
+
+#define LOWER MO(LAYER_LOWER)
+#define RAISE MO(LAYER_RAISE)
+
+#define CTL_BSP CTL_T(KC_BSPC)
+#define SFT_SPC SFT_T(KC_SPC)
+#define GUI_ENT GUI_T(KC_ENT)
+
+#define KC_COPY LCTL(KC_C)
+#define KC_CUT LCTL(KC_X)
+#define KC_PEGAR LCTL(KC_V)
+#define KC_UNDO LCTL(KC_Z)
+#define KC_REDO LCTL(KC_Y)
+
+// MT modifiers
+#define LALT_R LALT_T(KC_R)
+#define RALT_I RALT_T(KC_I)
+#define LCTL_S LCTL_T(KC_S)
+#define RCTL_E RCTL_T(KC_E)
+#define LS_T LSFT_T(KC_T)
+#define RSFT_N RSFT_T(KC_N)
+// tap-hold
+#define Y_OR_ LT(0, KC_Y)
+#define O_MINS LT(0, KC_O)
+#define H_DOWN LT(0, KC_H)
+#define COMMA_UP LT(0, KC_COMM)
+#define C_LEFT LT(0, KC_C)
+#define D_RIGHT LT(0, KC_D)
+
+// combos
+enum combo_events {
+    ESC,
+    NAV,
+    CAPS_LOCK,
+    BUFF_NEXT,
+    BUFF_PREV,
+    /*EMAIL,*/
+    COMBO_LENGTH
+};
+
+const uint16_t PROGMEM esc_combo[]    = {LCTL_S, RCTL_E, COMBO_END};
+const uint16_t PROGMEM to_nav_combo[] = {C_LEFT, COMMA_UP, COMBO_END};
+const uint16_t PROGMEM buffer_prev[]  = {LALT_R, LS_T, COMBO_END};
+const uint16_t PROGMEM buffer_next[]  = {RALT_I, RSFT_N, COMBO_END};
+const uint16_t PROGMEM caps_word[]    = {KC_F, KC_U, COMBO_END};
+const uint16_t PROGMEM email[]        = {KC_G, KC_M, COMBO_END};
+
+uint16_t COMBO_LEN = COMBO_LENGTH;
+
+combo_t key_combos[] = {
+    [ESC] = COMBO_ACTION(esc_combo), [NAV] = COMBO_ACTION(to_nav_combo), [CAPS_LOCK] = COMBO_ACTION(caps_word), [BUFF_NEXT] = COMBO_ACTION(buffer_next), [BUFF_PREV] = COMBO_ACTION(buffer_prev),
+    /*[EMAIL]= COMBO_ACTION(email),*/
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case ESC: {
+            if (pressed) {
+                tap_code16(KC_ESC);
+            }
+            break;
+        }
+        case NAV: {
+            if (pressed) {
+                layer_on(LAYER_NAV);
+            }
+            break;
+        }
+        case CAPS_LOCK: {
+            if (pressed) {
+                /*tap_code16(KC_CAPS_LOCK);*/
+                caps_word_on();
+            }
+            break;
+        }
+        // to be used in  vim to go to next buffer
+        case BUFF_NEXT: {
+            if (pressed) {
+                SEND_STRING(":bn");
+                tap_code16(KC_ENT);
+            }
+            break;
+        }
+        // to be used in  vim to go to previous buffer
+        case BUFF_PREV: {
+            if (pressed) {
+                SEND_STRING(":bp");
+                tap_code16(KC_ENT);
+            }
+            break;
+        }
+            /*case EMAIL: {*/
+            /*if (pressed){*/
+            /*SEND_STRING("neithanmo@gmail.com");*/
+            /*}*/
+            /*break;*/
+        /*}*/
+        default:
+            break;
+    }
+}
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [LAYER_BASE] = LAYOUT_split_3x5_2(
+  // ╭───────────────────────────────────────────────────────────╮ ╭───────────────────────────────────────────────────────────────────────────────╮
+       KC_Q,    KC_W,   KC_F,   KC_P,       KC_B,                    KC_J, KC_L,   KC_U,     Y_OR_,  KC_SCLN,
+  // ├───────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────────────────────────┤
+       KC_A,    LALT_R, LCTL_S, LS_T,    KC_G,                       KC_M, RSFT_N, RCTL_E,   RALT_I, O_MINS,
+  // ├───────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────────────────────────┤
+       KC_Z,    KC_X,   C_LEFT, D_RIGHT,    KC_V,                    KC_K, H_DOWN, COMMA_UP, KC_DOT, KC_SLSH,
+  // ╰───────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────────────────────────╯
+        LT(LAYER_NUM, KC_ENT),  LT(LAYER_SYM, KC_TAB),                             KC_LWIN, LT(LAYER_SYM,KC_SPC)
+  // ╰───────────────────────────────────────────────────────╯                  ╰────────────────────────────────────╯
+  ),
+
+  [LAYER_SYM] = LAYOUT_split_3x5_2(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────────────╮
+       KC_BSPC, KC_AT, KC_HASH, KC_DLR, KC_PERC,        KC_LBRC,    KC_LPRN,    KC_RPRN, KC_RBRC, KC_BSPC,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────────────┤
+       KC_TAB, KC_PEQL, KC_DQUO, KC_QUOT, KC_BSLS,     KC_AMPR,    KC_LCBR,    KC_RCBR, KC_ASTR, KC_ENT,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────────────┤
+       KC_TILD, KC_GRV, KC_LEFT, KC_RGHT, KC_PIPE,     KC_CIRC,    KC_DOWN,    KC_UP,   KC_EXLM, TO(4),
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────────────╯
+        KC_BSPC, XXXXXXX,                               KC_DEL, XXXXXXX
+  //    ╰───────────────────────╯                     ╰──────────────────╯
+  ),
+
+  [LAYER_NAV] = LAYOUT_split_3x5_2(
+  // ╭─────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────╮
+       KC_ESC, KC_WH_L, KC_PGUP, KC_PGDN, KC_WH_R,    KC_CUT,  KC_UNDO, LCTL(LSFT(KC_TAB)), RCTL(KC_TAB), KC_BSPC,
+  // ├─────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────┤
+       KC_TAB, KC_BTN1, KC_WH_U, KC_WH_D, XXXXXXX,    XXXXXXX, KC_COPY, KC_PEGAR,           KC_REDO,      KC_ENT,
+  // ├─────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────┤
+       XXXXXXX, XXXXXXX, KC_LEFT,  KC_RGHT, KC_DEL,   XXXXXXX, KC_DOWN, KC_UP,              XXXXXXX,      TO(LAYER_FUN),
+  // ╰─────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────╯
+           TO(LAYER_BASE), KC_BTN2,                          KC_LALT, KC_ENT
+  //     ╰───────────────────────────────────────╯       ╰──────────────────────╯
+  ),
+
+  [LAYER_NUM] = LAYOUT_split_3x5_2(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+       KC_ESC, KC_MPRV, KC_MPLY, KC_MNXT, KC_UNDS,    KC_MINS, KC_7,    KC_8,    KC_9,    KC_BSPC,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_TAB,   KC_MUTE, KC_VOLU, KC_VOLD, KC_DOT,   KC_PEQL, KC_4,    KC_5,    KC_6,    KC_SPC,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_PSLS, KC_PAST, KC_BRIU,  KC_BRID, KC_PLUS,  KC_0,    KC_1,    KC_2,    KC_3,    KC_PLUS,
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+             XXXXXXX, KC_ENT,                              KC_LALT,    KC_DOT
+  //        ╰───────────────────────────╯               ╰───────────────────────╯
+  ),
+
+  [LAYER_FUN] = LAYOUT_split_3x5_2(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+       KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_F5,   KC_F6, KC_F7, KC_F8,  QK_REBOOT,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_F1, KC_F2, KC_F3,  KC_F4,  XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, TO(LAYER_NUM),
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+            TO(LAYER_BASE), XXXXXXX,                       TO(LAYER_SYM), TO(LAYER_NAV)
+  //      ╰───────────────────────────────────╯          ╰───────────────────────────────╯
+  ),
+};
+
+// clang-format on
+
+#if defined(POINTING_DEVICE_ENABLE) && defined(DILEMMA_AUTO_SNIPING_ON_LAYER)
+layer_state_t layer_state_set_kb(layer_state_t state) {
+    state = layer_state_set_user(state);
+    dilemma_set_pointer_sniping_enabled(layer_state_cmp(state, DILEMMA_AUTO_SNIPING_ON_LAYER));
+    return state;
+}
+#endif // POINTING_DEVICE_ENABLE && DILEMMA_AUTO_SNIPING_ON_LAYER
+//
+static bool process_tap_or_long_press_key(keyrecord_t* record, uint16_t long_press_keycode) {
+    if (record->tap.count == 0) { // Key is being held.
+        if (record->event.pressed) {
+            register_code16(long_press_keycode);
+        } else {
+            unregister_code16(long_press_keycode);
+        }
+        return false; // Skip default handling.
+    }
+    return true; // Continue default handling.
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case Y_OR_: // Y on tap and _ on long press.
+            return process_tap_or_long_press_key(record, KC_UNDS);
+        case O_MINS: // Comma on tap, Ctrl+C on long press.
+            return process_tap_or_long_press_key(record, KC_MINS);
+        case H_DOWN: // Comma on tap, Ctrl+C on long press.
+            return process_tap_or_long_press_key(record, KC_DOWN);
+        case COMMA_UP: // Comma on tap, Ctrl+C on long press.
+            return process_tap_or_long_press_key(record, KC_UP);
+        case D_RIGHT: // Comma on tap, Ctrl+C on long press.
+            return process_tap_or_long_press_key(record, KC_RIGHT);
+        case C_LEFT: // Comma on tap, Ctrl+C on long press.
+            return process_tap_or_long_press_key(record, KC_LEFT);
+    }
+
+    return true;
+}
